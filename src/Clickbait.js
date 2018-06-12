cc9fcaa385731f227ae37070c7c9d3d79ea19d5b
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-12 08:51:52 -0500

    Bring back clickbait components and map those instead

--- src/Clickbait.js
+++ src/Clickbait.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import SingleOrcs from './SingleOrcs'
 import Mountain from './Mountain'
+import Suprise from './Surprise'
 import WeirdTrick from './WeirdTrick'
-import Surprise from './Surprise'
 
 class Clickbait extends Component {
     constructor (props) {
@@ -11,30 +11,22 @@ class Clickbait extends Component {
         const objArray = []
 
         const obj1 = {
-            kind: "small-6 medium-3 columns other-article",
-            img: <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc"/>,
-            p: <p>Single Orcs in Indianapolis</p>,
+            component: <SingleOrcs/>,
         }
         objArray[0] = obj1;
 
         const obj2 = {
-            kind: "small-6 medium-3 columns other-article",
-            img: <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain"/>,
-            p: <p>You won\'t believe what\'s under this mountain</p>,
+            component: <WeirdTrick/>
         }
         objArray[1] = obj2;
 
         const obj3 = {
-            kind: "small-6 medium-3 columns other-article", 
-            img: <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold"/>,
-            p: <p>Mine 20% more gold with One Weird Trick</p>,
+            component: <Mountain/>
         }
         objArray[2] = obj3;
 
         const obj4 = {
-            kind: "small-6 medium-3 columns other-article",
-            img: <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit"/>,
-            p: <p>Surprise for Indiana Hobbits born before 1999</p>,
+            component: <Suprise/>
         }
         objArray[3] = obj4;
 
@@ -45,11 +37,8 @@ class Clickbait extends Component {
             <div className="small-12 columns other-articles">
                 <h2>From around the Realm</h2>
                     {this.state.objArray.map((obj) => (
-                        <div className={obj.kind}>
-                            <a href='#'>
-                                {obj.img}
-                                {obj.p}
-                            </a>
+                        <div>
+                            {obj.component}
                         </div>
                     ))}
             </div>
@@ -58,9 +47,3 @@ class Clickbait extends Component {
 }
 
 export default Clickbait
-/*
-<SingleOrcs />
-<Mountain />
-<WeirdTrick />
-<Surprise />
-                    */
