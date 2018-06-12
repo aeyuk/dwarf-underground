64be5f81cf05025710557e7b27410096405f2273
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-12 10:33:08 -0500

    Add comments to page

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react'
 
 class ArticleLinks extends Component {
     commentBox() {
-      const input = document.getElementById('commentBox')
-      input.style.display = 'block'
+      const textBox = document.getElementById('comment')
+      textBox.style.display = 'block'
+      
+      document.querySelector('button').style.display = 'block'
     }
+
+    addComment() {
+      const textBox = document.getElementById('comment')
+      const input = document.createTextNode('')
+      const lineBreak = document.createElement('br')
+
+      input.textContent = textBox.value;
+
+      const listEl = document.getElementById('comments')
+      listEl.appendChild(input)
+      listEl.appendChild(lineBreak)
+
+      //reset
+      textBox.style.display = 'none'
+      document.querySelector('button').style.display = 'none'
+      textBox.value = "";
+
+    }
+
     render () {
         return (
           <div className="article-links">
@@ -12,7 +33,12 @@ class ArticleLinks extends Component {
               <i className="fa fa-comments-o"></i>
               <span className="article-link-text">Comments</span>
             </a>
-            <input type="text" id="commentBox" style={{display: 'none'}}></input>
+            <div>
+              <ul id="comments">
+                <input type="text" id="comment" style={{display: 'none'}}></input>
+                <button type='submit' style={{display: 'none'}} onClick={() => this.addComment()}>Submit Comment</button>
+              </ul>
+            </div>
             <a className="article-link" href="#">
               <i className="fa fa-share"></i>
               <span className="article-link-text">Share Post</span>
